e360e0386a3d2f8d07e8af2c2805bdacaf7190c6
shihchinghuang <[huang832047@gmail.com]>
2021-07-06 10:07:36 +0800

    取不到單筆資料

--- src/pages/article/ArticleDetail.js
+++ src/pages/article/ArticleDetail.js
@@ -10,7 +10,7 @@ import ProgressBar from "react-scroll-progress-bar";
 import { RiBookmarkFill } from "react-icons/ri";
 import { TiSocialFacebook } from "react-icons/ti";
 import { AiOutlineInstagram } from "react-icons/ai";
-import Navi from "./components/Navi.js";
+// import Navi from "./components/Navi.js";
 
 // const sessionServer = async () => {
 //   const url = `http://localhost:6005/article`;
@@ -27,7 +27,7 @@ import Navi from "./components/Navi.js";
 //   setGetSession(data);
 // };
 
-const ArticleDetail = () => {
+const ArticleDetail = (prop) => {
   const [article, setArticle] = useState([]);
   const [dataLoading, setDataLoading] = useState(false);
   var moment = require("moment");
@@ -37,7 +37,7 @@ const ArticleDetail = () => {
     setDataLoading(true);
 
     // 連接的伺服器資料網址
-    const url = "http://localhost:6005/article";
+    const url = "http://localhost:6005/article/" + id;
 
     // 注意header資料格式要設定，伺服器才知道是json格式
     const request = new Request(url, {
@@ -67,67 +67,68 @@ const ArticleDetail = () => {
   }, [article]);
 
   return (
-    article.length &&
-    article.map((v, i) => {
-      return (
-        <>
-          <Navi />
-          <div>
-            <ProgressBar height="15px" bgcolor="#FDD2BB" />
-            {/* <div className="wrap"> 
+    <>
+      {article?.data?.map((v, i) => {
+        return (
+          <>
+            {/* <Navi /> */}
+            <div>
+              <ProgressBar height="15px" bgcolor="#FDD2BB" />
+              {/* <div className="wrap"> 
           <div className="box" />
         </div> */}
-            <div className="container">
-              <img src="./img/Image 17.png" className="w-100" alt="" />
-              <div className="title">
-                <h3>{v.articleName}</h3>
-                <img src="./bg-svg/Path 534.svg" alt="" />
-                <div className="author-date">
-                  <p className="m-0">{v.articleAuthor}</p>
-                  <small className="m-0">
-                    {moment(v.articleDate).format("YYYY-MM-DD")}
-                  </small>
+              <div className="container">
+                <img src="./img/Image 17.png" className="w-100" alt="" />
+                <div className="title">
+                  <h3>{v.articleName}</h3>
+                  <img src="./bg-svg/Path 534.svg" alt="" />
+                  <div className="author-date">
+                    <p className="m-0">{v.articleAuthor}</p>
+                    <small className="m-0">
+                      {moment(v.articleDate).format("YYYY-MM-DD")}
+                    </small>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="container">
-              <div className="d-flex justify-content-between">
-                <div className="ml-4">
-                  <ArticleDetailJquery />
+              <div className="container">
+                <div className="d-flex justify-content-between">
+                  <div className="ml-4">
+                    <ArticleDetailJquery />
+                  </div>
+                  <div className="mr-4">
+                    <RiBookmarkFill className="h4 mr-4" />
+                    <TiSocialFacebook className="h3 mr-4" />
+                    <AiOutlineInstagram className="h3 mr-4" />
+                  </div>
                 </div>
-                <div className="mr-4">
-                  <RiBookmarkFill className="h4 mr-4" />
-                  <TiSocialFacebook className="h3 mr-4" />
-                  <AiOutlineInstagram className="h3 mr-4" />
+                <div className="content">
+                  <img src="./bg-svg/Group 1120.png" alt="" />
+                  <p>{v.articleContent}</p>
                 </div>
               </div>
-              <div className="content">
-                <img src="./bg-svg/Group 1120.png" alt="" />
-                <p>{v.articleContent}</p>
-              </div>
-            </div>
-            <div className="article-recommand container mb-5">
-              <div className="container d-flex justify-content-between align-items-center border-bottom border-dark mb-5">
-                <div>
-                  <h3>相關文章</h3>
-                </div>
+              <div className="article-recommand container mb-5">
+                <div className="container d-flex justify-content-between align-items-center border-bottom border-dark mb-5">
+                  <div>
+                    <h3>相關文章</h3>
+                  </div>
 
-                <div>
-                  <p className="btn-more mt-3 mx-auto" href="#">
-                    more
-                  </p>
+                  <div>
+                    <p className="btn-more mt-3 mx-auto" href="#">
+                      more
+                    </p>
+                  </div>
                 </div>
+                <ArticleCard />
               </div>
-              <ArticleCard />
             </div>
-          </div>
 
-          <div className="container">
-            <TodoApp />
-          </div>
-        </>
-      );
-    })
+            <div className="container">
+              <TodoApp />
+            </div>
+          </>
+        );
+      })}
+    </>
   );
 };
 
